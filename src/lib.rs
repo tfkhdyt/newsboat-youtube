5690addb5c0f00ffe35907ebf4eae64484144988
tfkhdyt <tfkhdyt@proton.me>
2023-02-17 13:59:54 +0700

    fix(parse handle): handle error when failed to get channel id and name

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
+use serde_json::Value;
 use std::fs::OpenOptions;
 use std::io::Write;
-
-use serde_json::Value;
 
 pub fn parse_handle(url: String) -> Result<String, String> {
     if !url.contains('@') {
@@ -35,8 +34,12 @@ pub async fn fetch_yt_api(
     let resp = reqwest::get(format!("https://youtube.googleapis.com/youtube/v3/search?part=id,snippet&q={yt_handle}&type=channel&key={api_key}"))
         .await?.json::<Value>().await?;
 
-    let channel_id = &resp["items"][0]["id"]["channelId"].as_str().unwrap();
-    let channel_name = &resp["items"][0]["snippet"]["title"].as_str().unwrap();
+    let channel_id = &resp["items"][0]["id"]["channelId"]
+        .as_str()
+        .ok_or("Failed to get channel id")?;
+    let channel_name = &resp["items"][0]["snippet"]["title"]
+        .as_str()
+        .ok_or("Failed to get channel name")?;
 
     Ok((channel_id.to_string(), channel_name.to_string()))
 }
