ebd97c555c573d870e703c3dcb19150833add2a4
tfkhdyt <tfkhdyt@proton.me>
2023-02-21 12:52:58 +0700

    feat: apply rayon on no confirmation

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::process;
 use args::{Cli, Commands};
 use clap::Parser;
 use config::Config;
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use subcommands::add;
 
 fn main() {
@@ -21,17 +22,31 @@ fn main() {
         Some(Commands::Add {
             urls,
             no_confirmation,
-        }) => urls.iter().for_each(|url| {
-            let result = match add::execute(url, &api_key, filename, *no_confirmation) {
-                Ok(v) => v,
-                Err(err) => {
-                    eprintln!("Error: {}", err);
-                    process::exit(1);
-                }
-            };
-
-            println!("{}\n", result);
-        }),
+        }) => {
+            if *no_confirmation {
+                urls.par_iter().for_each(|url| {
+                    let result = match add::execute(url, &api_key, filename, *no_confirmation) {
+                        Ok(v) => v,
+                        Err(err) => {
+                            eprintln!("Error: {}", err);
+                            process::exit(1);
+                        }
+                    };
+                    println!("{}", result);
+                });
+            } else {
+                urls.iter().for_each(|url| {
+                    let result = match add::execute(url, &api_key, filename, *no_confirmation) {
+                        Ok(v) => v,
+                        Err(err) => {
+                            eprintln!("Error: {}", err);
+                            process::exit(1);
+                        }
+                    };
+                    println!("{}", result);
+                });
+            }
+        }
         None => {}
     }
 }
